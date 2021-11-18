1dcefd29d1c38b84b1f969bdec96897b231eecea
1Yolanda <yolandabermeotrujillo@gmail.com>
2021-11-18 11:57:21 -0500

    clase nueva

---
+++ Javascript/js/12_operadoreslogicos2.js
@@ -0,0 +1,9 @@
+var jovenesAccionUsusario = true;
+var sisbenUsuariov = true;
+
+if ( jovenesAccionUsusario === true ^ sisbenUsuariov === true) {
+    console.log("usted puede accesde al beneficio de transporte");
+}
+else {
+    console.log("No puede acceder al beneficio de transporte.");
+}
