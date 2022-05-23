b101eb9a2313b38e576445b16e36f1cb7bfaba02
nicolas <sebascruzmusic@gmail.com>
2022-05-23 08:49:21 -0500

    amazon 2

--- Amazon/js/script.js
+++ Amazon/js/script.js
@@ -1,5 +1,41 @@
 "use strict"
 
-function pruebaSume() {
+let cantidadProductosGlobal = document.getElementById("cantidadProductos").innerHTML;
+
+function sumar() {
+    if (cantidadProductosGlobal < 10) {
+    cantidadProductosGlobal = Number(cantidadProductosGlobal) + 1;
+    document.getElementById("cantidadProductos").innerHTML = cantidadProductosGlobal;
+    modificarTotal();
+    }
+}
+function restar() {
+    if (cantidadProductosGlobal > 0){
+    cantidadProductosGlobal = Number(cantidadProductosGlobal) - 1;
+    document.getElementById("cantidadProductos").innerHTML = cantidadProductosGlobal;
+    modificarTotal();
+    }
+}
+function modificarTotal() {
+    let cantidadProductos = document.getElementById("cantidadProductos").innerHTML;
+    if (cantidadProductos == "1") {
+        document.getElementById("cantidadProductos").innerHTML;
+        
+    }
+    else {
+        document.getElementById("cantidadProductos").innerHTML = cantidadProductos;
+    }
+    mostrarTotal();
+}
+function mostrarTotal() {
     
+    let valorUnitario = document.getElementById("valorUnitario").innerHTML;
+    let valorSubtotal = Number(valorUnitario) * Number(cantidadProductosGlobal);
+    document.getElementById("valorSubtotal").innerHTML = valorSubtotal;
+}
+function realizar() {
+    alert ("compra realizada");
+}
+function eliminar() {
+    alert ("Compra cancelada");
 }
